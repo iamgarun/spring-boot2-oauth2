605f7d2e81bc9762e48c0f821bae517473e60b96
Arun Kumar Gopikaramanan <iamgarun@users.noreply.github.com>
2019-04-13 18:54:11 +0530

    Modified protected endpoints in ResourceConfig

--- src/main/java/com/smedzl/example/config/ResourceServerConfig.java
+++ src/main/java/com/smedzl/example/config/ResourceServerConfig.java
@@ -12,7 +12,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.requestMatchers().antMatchers("/user").and().authorizeRequests().anyRequest().authenticated();
+		http.requestMatchers().antMatchers("/profile").and().authorizeRequests().anyRequest().authenticated();
 	}
 
 	@Override
